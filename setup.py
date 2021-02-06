50bfaff18b9ccb0f6c7304da702755be50cd63fb
Francisco Pérez <frapercan1@alum.us.es>
2021-02-06 12:29:19 +0100

    initialize package with tests

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
+
 def read(filename):
     filename = os.path.join(os.path.dirname(__file__), filename)
     text_type = type(u"")
@@ -23,6 +24,7 @@ setup(
     author_email="frapercan1@alum.us.es",
 
     description="Python bitfinex scrapping functions. Storing results into InfluxDB",
+    long_description_content_type="text/x-rst",
     long_description=read("README.rst"),
 
     packages=find_packages(exclude=('tests',)),
@@ -32,13 +34,6 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
