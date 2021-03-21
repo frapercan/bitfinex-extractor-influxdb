18d2213c996aa7ec2c76c824a881e53a031e541a
Francisco Pérez <frapercan1@alum.us.es>
2021-03-21 13:09:00 +0100

    added logger

--- tests/test_multiple_stocks_sync.py
+++ tests/test_multiple_stocks_sync.py
@@ -106,7 +106,8 @@ mock_url_generated = "https://api-pub.bitfinex.com/v2/candles/trade:12h:tBTCUSD/
                      "&sort=1 "
 mock_last_sample_timestamp = 1613908800
 
-#Simulating two correct iterations, the second one will be up to date.
+
+# Simulating two correct iterations, the second one will be up to date.
 
 
 @patch('influxdb_client.client.write_api.WriteApi.write',
@@ -115,7 +116,7 @@ mock_last_sample_timestamp = 1613908800
        MagicMock(side_effect=[False, True]))
 @patch('bitfinex_extractor_influxdb.multiple_stocks_sync.DataSync._check_bitfinex_connection',
        MagicMock(return_value=True))
-@patch('requests.get', MagicMock(return_value=pickle.load(open("./tests/json_response.p", "rb"))))
+@patch('requests.get', MagicMock(return_value=pickle.load(open("./tests/bitfinex_response_candle.p", "rb"))))
 @patch('bitfinex_extractor_influxdb.multiple_stocks_sync.url_generator',
        MagicMock(return_value=mock_url_generated))
 @patch('bitfinex_extractor_influxdb.multiple_stocks_sync.DataSync._get_last_sample_timestamp',
@@ -130,3 +131,97 @@ def test_extract_series():
     assert sync.client.write_api().write.call_count == 1
 
 
+# Bitfinex connection not stablished at first attempt, it will finish sync at second try.
+@patch('influxdb_client.client.write_api.WriteApi.write',
+       MagicMock(return_value=False))
+@patch('bitfinex_extractor_influxdb.multiple_stocks_sync.compare_timestamps',
+       MagicMock(return_value=True))
+@patch('bitfinex_extractor_influxdb.multiple_stocks_sync.DataSync._check_bitfinex_connection',
+       MagicMock(side_effect=[False, True]))
+@patch('requests.get', MagicMock(return_value=pickle.load(open("./tests/bitfinex_response_candle.p", "rb"))))
+@patch('bitfinex_extractor_influxdb.multiple_stocks_sync.url_generator',
+       MagicMock(return_value=mock_url_generated))
+@patch('bitfinex_extractor_influxdb.multiple_stocks_sync.DataSync._get_last_sample_timestamp',
+       MagicMock(return_value=mock_last_sample_timestamp))
+def test_no_connection():
+    sync = test_initialize()
+    sync._extract_series(pair_test, timeframe_test)
+    assert sync._get_last_sample_timestamp.call_count == 1
+    assert multiple_stocks_sync.url_generator.call_count == 2
+    assert sync._check_bitfinex_connection.call_count == 2
+    assert multiple_stocks_sync.compare_timestamps.call_count == 1
+    assert sync.client.write_api().write.call_count == 0
+
+
+# Write call throws exception, first attempt. 'last_sample_timestamp_ns' shouldn't be updated that time.
+@patch('influxdb_client.client.write_api.WriteApi.write',
+       MagicMock(side_effect=[Exception('Test'), True]))
+@patch('bitfinex_extractor_influxdb.multiple_stocks_sync.compare_timestamps',
+       MagicMock(side_effect=[False, False, True, True]))
+@patch('bitfinex_extractor_influxdb.multiple_stocks_sync.DataSync._check_bitfinex_connection',
+       MagicMock(return_value=True))
+@patch('requests.get', MagicMock(return_value=pickle.load(open("./tests/bitfinex_response_candle.p", "rb"))))
+@patch('bitfinex_extractor_influxdb.multiple_stocks_sync.url_generator',
+       MagicMock(return_value=mock_url_generated))
+@patch('bitfinex_extractor_influxdb.multiple_stocks_sync.DataSync._get_last_sample_timestamp',
+       MagicMock(return_value=mock_last_sample_timestamp))
+def test_write_call_exception():
+    sync = test_initialize()
+    sync._extract_series(pair_test, timeframe_test)
+    assert sync._get_last_sample_timestamp.call_count == 1
+    assert multiple_stocks_sync.url_generator.call_count == 3
+    assert sync._check_bitfinex_connection.call_count == 3
+    assert multiple_stocks_sync.compare_timestamps.call_count == 3
+    assert sync.client.write_api().write.call_count == 2
+
+    assert multiple_stocks_sync.url_generator.call_args_list[-2] != multiple_stocks_sync.url_generator.call_args_list[
+        -1]
+
+
+@patch('influxdb_client.client.query_api.QueryApi.query_data_frame',
+       MagicMock(return_value=pickle.load(open("./tests/last_sample_query.p", "rb"))))
+def test_get_last_sample_timestamp():
+    sync = test_initialize()
+    print(pickle.load(open("./tests/last_sample_query.p", "rb")))
+    assert sync._get_last_sample_timestamp(pair_test, timeframe_test) == \
+           pickle.load(open("./tests/last_sample_query.p", "rb"))['_time'][0].timestamp()
+
+
+@patch('influxdb_client.client.query_api.QueryApi.query_data_frame',
+       MagicMock(side_effect=KeyError('initialize timeserie')))
+def test_get_last_sample_timestamp_key_exception():
+    sync = test_initialize()
+    assert sync._get_last_sample_timestamp(pair_test, timeframe_test) == sync.timeseries_start.timestamp()
+
+
+def test_check_bitfinex_connection():
+    sync = test_initialize()
+    assert sync._check_bitfinex_connection(pickle.load(open("./tests/bitfinex_response_candle.p", "rb"))) == True
+
+
+@patch('time.sleep', MagicMock(side_effect=None))
+def test_check_bitfinex_connection_limit_rate():
+    sync = test_initialize()
+    assert sync._check_bitfinex_connection(pickle.load(open("./tests/bitfinex_response_limit_error.p", "rb"))) == False
+
+
+@patch('time.sleep', MagicMock(side_effect=None))
+def test_check_bitfinex_connection_maintenance():
+    sync = test_initialize()
+    assert sync._check_bitfinex_connection(['error', multiple_stocks_sync.ERROR_CODE_START_MAINTENANCE]) == False
+
+
+url_generator_pair = 'tBTCUSD'
+url_generator_timeframe = '14D'
+url_generator_last_sample_timestamp_ns = '1616025600000'
+mock_url_generator_expected = 'https://api-pub.bitfinex.com/v2/candles/trade:14D:tBTCUSD/hist?limit=1000&start=1616025600000&sort=1'
+
+
+def test_url_generator():
+    assert multiple_stocks_sync.url_generator(url_generator_pair, url_generator_timeframe,
+                                              url_generator_last_sample_timestamp_ns) == mock_url_generator_expected
+
+
+def test_compare_timestamp():
+    assert multiple_stocks_sync.compare_timestamps(url_generator_last_sample_timestamp_ns,
+                                                   url_generator_last_sample_timestamp_ns) == True
